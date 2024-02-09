2a3da55b5d136194431deac7f35997edbeff424a
Trigo <trigoyodila1996@gmail.com>
2024-02-09 17:42:51 +0100

    added namespaces

--- src/public/index.php
+++ src/public/index.php
@@ -21,3 +21,18 @@ $transaction = new Transaction(100,'Transaction 1');
 $transaction->addTax(8)->applyDiscount(10);
 
 var_dump($transaction->getAmount());
+
+echo '</br></br>';
+
+// Namespaces
+require_once '../PaymentGateway/Stripe/Transaction.php';
+require_once '../PaymentGateway/Paddle/Transaction.php';
+
+//utilisation de use pour importer l'espace de nom
+use PaymentGateway\Paddle\Transaction;
+
+//utilise la classe se trouvant dans le namespace précisé
+var_dump(new PaymentGateway\Stripe\Transaction());
+
+//utiliser avec use
+var_dump(new Transaction());
