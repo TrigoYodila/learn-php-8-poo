9e3c04c7a96beae3d396880a4ef6da5822f03910
Trigo <trigoyodila1996@gmail.com>
2024-02-28 17:17:27 +0100

    refactor routing with get and post super globals

--- src/app/Classes/Home.php
+++ src/app/Classes/Home.php
@@ -8,11 +8,23 @@ class Home
 {
     public function index():string
     {
-        return 'Home';
+        // return 'Home';
+
+        // echo '<pre>';
+        //    var_dump($_REQUEST);
+        // echo '</pre>';
+
+        // echo '<pre>';
+        //     var_dump($_GET);
+        // echo '</pre>';
+
+        // echo '<pre>';
+        //     var_dump($_POST);
+        // echo '</pre>';
+
+        return '<form action="/?foo=bar&amount=250" method="post"><label>Amount</label><input type="text" name="amount" /></form>';
+
     }
 
-    public function create():string 
-    {
-        return 'Create Invoice';
-    }
+    
 }
--- src/public/index.php
+++ src/public/index.php
@@ -12,9 +12,10 @@ $router = new App\Router();
 // );
 
 $router
-    ->register('/', [App\Classes\Home::class,'index'])
-    ->register('/invoices', [App\Classes\Invoice::class, 'index'])
-    ->register('/invoices/create', [App\Classes\Invoice::class, 'create']);
+    ->get('/', [App\Classes\Home::class,'index'])
+    ->get('/invoices', [App\Classes\Invoice::class, 'index'])
+    ->get('/invoices/create', [App\Classes\Invoice::class, 'create'])
+    ->post('/invoices/create', [App\Classes\Invoice::class, 'create']);
 
 // $router->register(
 //     '/invoices',
@@ -23,4 +24,4 @@ $router
 //     }
 // );
 
-echo $router->resolve($_SERVER['REQUEST_URI']);
+echo $router->resolve($_SERVER['REQUEST_URI'], strtolower($_SERVER['REQUEST_METHOD']));
