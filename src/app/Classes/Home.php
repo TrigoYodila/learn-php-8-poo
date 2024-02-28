ac60ef8091aca6fd9562f1446a977c32aec038c0
Trigo <trigoyodila1996@gmail.com>
2024-02-28 13:57:53 +0100

    creation d'un routage simple fromscratch

---
+++ src/app/Classes/Home.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Classes;
+
+class Home
+{
+    public function index():string
+    {
+        return 'Home';
+    }
+
+    public function create():string 
+    {
+        return 'Create Invoice';
+    }
+}
--- src/public/index.php
+++ src/public/index.php
@@ -1,3 +1,26 @@
 <?php 
 
 require_once __DIR__ . '/../vendor/autoload.php';
+
+$router = new App\Router();
+
+// $router->register(
+//     '/',
+//     function (){
+//         echo 'Home';
+//     }
+// );
+
+$router
+    ->register('/', [App\Classes\Home::class,'index'])
+    ->register('/invoices', [App\Classes\Invoice::class, 'index'])
+    ->register('/invoices/create', [App\Classes\Invoice::class, 'create']);
+
+// $router->register(
+//     '/invoices',
+//     function(){
+//         echo 'Invoices';
+//     }
+// );
+
+echo $router->resolve($_SERVER['REQUEST_URI']);
