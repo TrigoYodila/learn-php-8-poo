8c566fbf95a0dc4633654ad41e8cf41b09b87a75
Trigo <trigoyodila1996@gmail.com>
2024-03-09 20:43:32 +0100

    utilisation des espaces reservés et paramètres nommés

--- src/app/Controllers/HomeController.php
+++ src/app/Controllers/HomeController.php
@@ -15,12 +15,77 @@ class HomeController
 
        // connect to db
        try {
-         $db = new PDO('mysql:host=db;dbname=my_db','root','root');
+         $db = new PDO('mysql:host=db;dbname=my_db','root','root', [
+            //mode de recuperation des données
+           // PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
+         ]);
+
+         //defined query
+         //$query = 'SELECT * FROM users';
+         //$email = $_GET['email'];
+         //$query = 'SELECT * FROM users WHERE email = "' . $email . '"';
+
+         $email = 'zephy@yod.com';
+         $name = 'Zephy Manya ';
+         $isActive = 1;
+         $createdAt = date('Y-m-d H:m:i', strtotime('07/03/2024 9:00PM'));
+
+        //utilisation d'espace renversé
+        //  $query = 'INSERT INTO users (email, full_name, is_active, created_at)
+        //            VALUES (?,?,?,?)';
+         
+        //utilisation des paramètres nommés
+        $query = 'INSERT INTO users (email, full_name, is_active, created_at)
+                  VALUES (:email,:name,:active,:date)';
+         
+         //prepare request
+         $stmt = $db->prepare($query);
+
+         //$stmt->execute([$email]);
+        //$stmt->execute([$email, $name, $isActive, $createdAt]);
+
+        // paramètre nommé
+        // $stmt->execute(
+        //     [
+        //         'name' => $name,
+        //         'email' => $email,
+        //         'active' => $isActive,
+        //         'date' => $createdAt
+        //     ]
+        //     );
+
+        // utilisation de bindValue + paramètre nommée
+        $stmt->bindValue('name', $name);
+        $stmt->bindValue('email', $email);
+        $stmt->bindValue('active', $isActive, PDO::PARAM_BOOL);
+        $stmt->bindValue('date', $createdAt);
+
+        $stmt->execute();
+
+        //recuperation du dernier user
+        $id = (int) $db->lastInsertId();
+
+        $user = $db->query('SELECT * FROM users WHERE id= ' . $id)->fetch();
+
+        echo '<pre>';
+        var_dump($user);
+        echo '</pre>';
+
+        //  foreach ($db->query($query) as $user) {
+        //     echo '<pre>';
+        //     var_dump($user);
+        //     echo '<pre/>';
+        //  }
+        // foreach ($stmt->fetchAll() as $user) {
+        //     echo '<pre>';
+        //     var_dump($user);
+        //     echo '<pre/>';
+        // }
+
        } catch (\PDOException $e) {
         throw new \PDOException($e->getMessage(), $e->getCode());
        }
 
-       var_dump($db);
         return View::make('index');
     }
 
